db9de37726244d062faf65bd65a9c558c89e0d8b
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2026-04-11 15:10:18 +0700

    fix: login

--- src/features/Login/api/login.api.ts
+++ src/features/Login/api/login.api.ts
@@ -1,6 +1,6 @@
 import axiosClient, { globalConfig } from "@/shared/api";
 
-const LOGIN_ENDPOINT = "auth/login";
+const LOGIN_ENDPOINT = "/auth/login";
 
 interface LoginPayload {
   username: string;
@@ -8,8 +8,8 @@ interface LoginPayload {
 }
 
 interface LoginResponse {
-  accessToken?: string;
-  refreshToken?: string;
+  access_token?: string;
+  refresh_token?: string;
   message?: string;
 }
 
